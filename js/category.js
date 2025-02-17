679ea32bc9c2c955ba654f43b98648b0e2b019a2
Sarah Timann <saol0003@stud.kea.dk>
2025-02-17 12:00:50 +0100

    add changes in js

--- js/category.js
+++ js/category.js
@@ -4,22 +4,16 @@ const categoryContainer = document.querySelector(".grid_1-1-1-1");
 fetch("https://dummyjson.com/recipes")
   .then((response) => response.json())
   .then((data) => {
-    const recipes = data.recipes;
-
-    const filterRecipes = recipes.filter((recipe) => recipe.mealType.includes(categoryType));
-
-    categoryContainer.innerHTML = filterRecipes
-      .map(
-        (recipe) => `
+    categoryContainer.innerHTML = Recipes.map(
+      (recipe) => `
       <div class="category-img-1">
-        <a href="single.html?id=${recipe.id}">
+        <a href="list.html?id=${recipe.id}">
           <img src="${recipe.image}" class="img-1" alt="${recipe.name}" />
         </a>
         <div class="link-1">
-          <a href="single.html?id=${recipe.id}">${recipe.name}</a>
+          <a href="list.html?id=${recipe.id}">${recipe.name}</a>
         </div>
       </div>
     `
-      )
-      .join("");
+    ).join("");
   });
