61fd1f65cce883a7a71fedadc16a43bc7ab28d76
amanda andersen <aman0002@stud.kea.dk>
2025-02-18 19:02:01 +0100

    add fix for grid in single

--- js/single.js
+++ js/single.js
@@ -9,14 +9,18 @@ if (opskriftId) {
         <h1>${data.name}</h1>
         <img class="heroImage" src="${data.image}" alt="${data.name}" />
         <div class="grid_1-3">
+          <img src="assets/preptime.svg" alt="preptime ticon" />
           <p>Prep time: ${data.prepTimeMinutes} min.</p>
+          <img src="assets/cooktime.svg" alt="cooktime icon" />
           <p>Cook time: ${data.cookTimeMinutes} min.</p>
         </div>
         <div class="grid_1-1">
           <div class="ingredients">
             <h3>Ingredients:</h3>
             <ul>
-              ${data.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+              ${data.ingredients
+                .map((ingredient) => `<li>${ingredient}</li>`)
+                .join("")}
             </ul>
           </div>
           <div class="instructions">
